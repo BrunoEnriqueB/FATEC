53a4111a06a48eec6fce1f8da1308ed6eee79ce1
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-10-02 20:50:21 -0300

    Linguagem de Programacao

---
+++ 2_Semestre/Linguagem_de_Programacao/C/aulas/Aula_08-17_de_Setembro/ex01-sort_meu.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <locale.h>
+
+int ind_min(int *v, int b, int e) {
+    int menor = v[b];
+    int i = b;
+    for(int cont = b; cont < e; cont++) {
+        if (v[cont] < menor) {
+            menor = v[cont];
+            i = cont;
+        }
+    }
+    return i;
+}
+
+int sort(int *v, int n) {
+    for(int cont = 0; cont < n; cont++) {
+        int menor_id = ind_min(v, cont, n);
+        int troca = v[cont];
+        v[cont] = v[menor_id];
+        v[menor_id] = troca;
+
+    }
+}
+
+void main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int v[] = {3, 5, 1, 2, 4};
+
+    sort(v, sizeof(v)/4);
+
+    for(int i = 0; i < sizeof(v)/4; i++) {
+        printf("v[%d] = %d\n", i, v[i]);
+    }
+}
