649752f3d6d23b0b472f1d096e92aae2d2e98b05
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-10-06 22:14:53 -0300

    Sistemas da informação - Aula 11 - 06/10/2021 - VBNET + MYSQL

---
+++ 2_Semestre/Sistemas_da_Informacao/Visual Studio/Aula_11/cadastro_adsma2/cadastro_adsma2/cadastro_adsma2/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call conectar_banco()
+    End Sub
+
+    Private Sub btn_excluir_Click(sender As Object, e As EventArgs) Handles btn_excluir.Click
+
+    End Sub
+
+    Private Sub btn_buscar_Click(sender As Object, e As EventArgs) Handles btn_buscar.Click
+
+    End Sub
+
+    Private Sub btn_gravar_Click(sender As Object, e As EventArgs) Handles btn_gravar.Click
+        Try
+            sql = "select * from tb_clientes where cpf='" & txt_cpf.Text & "'"
+            rs = db.Execute(sql)
+            If rs.EOF = False Then
+                MsgBox("CPF: " & txt_cpf.Text & "" + vbNewLine &
+                   "Já cadastrado", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "ATENÇÃO")
+            Else
+                sql = "insert into tb_clientes values ('" & txt_cpf.Text & "','" & txt_nome.Text & "','" & txt_email.Text & "')"
+                rs = db.Execute(UCase(sql))
+                MsgBox("Cliente cadastrado com sucesso!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "AVISO")
+                limpar_cadastro()
+            End If
+        Catch ex As Exception
+            MsgBox("Erro ao gravar os dados!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "ATENÇÃO")
+        End Try
+    End Sub
+
+    Sub limpar_cadastro()
+        With Me
+            .txt_cpf.Clear()
+            .txt_nome.Clear()
+            .txt_email.Clear()
+            .txt_cpf.Focus()
+        End With
+    End Sub
+
+End Class
---
+++ 2_Semestre/Sistemas_da_Informacao/Visual Studio/Aula_11/cadastro_adsma2/cadastro_adsma2/cadastro_adsma2/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module Module1
+    Public db As New ADODB.Connection
+    Public rs As New ADODB.Recordset
+    Public sql As String
+
+    Sub conectar_banco()
+        Try
+            db = CreateObject("ADODB.Connection")
+            db.Open("Driver={MySQL ODBC 8.0 Unicode Driver};Server=localhost;PORT=3312;Database=cadastro_clientes_adsma2;User=root;Password=")
+            MsgBox("Conexão com Banco de Dados estabelecida!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "AVISO")
+        Catch ex As Exception
+            MsgBox("Conexão com Banco de Dados FALHA!", MsgBoxStyle.Information + MsgBoxStyle.OkOnly, "ATENÇÁO")
+        End Try
+    End Sub
+
+End Module
