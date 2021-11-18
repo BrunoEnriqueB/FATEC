8eedf7fc303e2c4cb183b2254202f73a7937cf51
BrunoEnriqueB <brunobaronenrique@gmail.com>
2021-11-18 17:38:10 -0300

    tarefa 3 para nota

---
+++ 2_Semestre/Linguagem_de_Programacao/C/para_nota/tarefa_3/ex05.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int vetor_perfeito(int *b, int *e) {
+    int vetor[e - b];
+    for (; b != e; ++b) {
+        
+    }
+}
+
+void main() {
+    int vetor [] = {4,2,5,1};
+    int n = sizeof(vetor)/sizeof(int);
+
+    vetor_perfeito(vetor, vetor + n);
+}
