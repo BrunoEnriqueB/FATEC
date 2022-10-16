4eac86ff096b9ae6068d9c24dff38f1a9170ffaf
brunoebaron <bruno@vixta.com>
2022-10-15 21:05:11 -0300

    Adicionar construtor privado para o singleton

--- 4_Semestre/Engenharia_Software/Singleton/src/main/java/Singleton.java
+++ 4_Semestre/Engenharia_Software/Singleton/src/main/java/Singleton.java
@@ -1,6 +1,8 @@
 public class Singleton {
     private static Singleton singleton_instance = null;
 
+    private Singleton() {}
+
     public static Singleton getInstance() {
         if(singleton_instance == null) {
             singleton_instance = new Singleton();
